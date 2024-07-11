e73fe90320447f02161f8f1c9aef8c819ec07928
jfang137 <yi.fang@naturali.io>
2024-07-11 08:28:40 +0800

    show models

--- frontend/src/components/views/builder/build.tsx
+++ frontend/src/components/views/builder/build.tsx
@@ -40,17 +40,17 @@ const BuildView = () => {
               key: "1",
               children: <SkillsView />,
             },
-            // {
-            //   label: (
-            //     <div className="w-full  ">
-            //       {" "}
-            //       <CpuChipIcon className="h-4 w-4 inline-block mr-1" />
-            //       Models
-            //     </div>
-            //   ),
-            //   key: "2",
-            //   children: <ModelsView />,
-            // },
+            {
+              label: (
+                <div className="w-full  ">
+                  {" "}
+                  <CpuChipIcon className="h-4 w-4 inline-block mr-1" />
+                  Models
+                </div>
+              ),
+              key: "2",
+              children: <ModelsView />,
+            },
             {
               label: (
                 <div className="w-full  ">
